ff77865b134fd78bb0d6fd29c6a77ff103a646b8
Manish Rathi <manishrathi4@gmail.com>
2020-06-24 14:12:34 +0530

    modified query processor

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -5,6 +5,8 @@ public class QueryProcessor {
     public String process(String query) {
         if (query.toLowerCase().contains("romeo and juliet")) {
             return "William Shakespeare";
+        } else if (query.toLowerCase().split(":")[1].contains("what is your name")) {
+            return "Manish Rathi";
         }
         return "";
     }
