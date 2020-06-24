7b7648f9b15eb7feeb0a3afc384b87ab5fcef723
Manish Rathi <manishrathi4@gmail.com>
2020-06-24 14:15:03 +0530

    query processor change

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -5,7 +5,7 @@ public class QueryProcessor {
     public String process(String query) {
         if (query.toLowerCase().contains("romeo and juliet")) {
             return "William Shakespeare";
-        } else if (query.toLowerCase().split(":")[1].contains("what is your name")) {
+        } else if (query.toLowerCase().contains("what is your name")) {
             return "Manish Rathi";
         }
         return "";
